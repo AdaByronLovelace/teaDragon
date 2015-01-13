f22c5e9a0ffba05e3dd1cead3d6601454ccd474b
Emily Smith <Emily.Smith@staples.com>
2015-01-13 08:37:46 -0800

    Adding all files of the website so far

---
+++ resources/js/tea.js
@@ -0,0 +1,33 @@
+$(document).ready( function() {
+    $(".sub-home").show();
+    $("#home").show();
+
+    $(".nav-item").click(function() {
+        $(".nav-item").removeClass("active");
+        $(this).addClass("active");
+        //hide sections and sub menus
+        $("main section").hide();
+        $(".content").hide();
+        $(".sub").hide();
+        if ($(this).attr("id") == "nav-home") {
+            $(".sub-home").show();
+            $("#home").show();
+        } else if ($(this).attr("id") == "nav-cal") {
+            $(".sub-cals").show();
+            $("#cal").show();
+        } else if ($(this).attr("id") == "nav-world") {
+            $(".sub-wrld").show();
+            $("#world-map").show();
+        } else if ($(this).attr("id") == "nav-sharn") {
+            $(".sub-shar").show();
+            $("#sharn-map").show();
+        } else if ($(this).attr("id") == "nav-char") {
+            $(".sub-char").show();
+            $("#characters").show();
+        }
+    });
+
+
+
+});
+
