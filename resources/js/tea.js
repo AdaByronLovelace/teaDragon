4b1cb77e96b3dbed7fe1dcc6fd22e2c24e98ca1f
OhMrSticky <Shogeji@gmail.com>
2015-02-19 14:01:04 -0800

    updating some css

--- resources/js/tea.js
+++ resources/js/tea.js
@@ -38,12 +38,42 @@ $(document).ready( function() {
         }
     });
     
-    $(".sub-shar a").click(function(e) {
+    $(".sub a").click(function(e) {
         e.preventDefault();
         $("html,body").animate({
             scrollTop: ($(this.attributes.href.value).offset().top)
         }, 1000);
     });
+    
+    
+    $(".nav-item").hover(function() {
+        $(".sub-menu").show()
+    }, function() {
+        $(".sub-menu").hide()
+    });
 
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+ 
+    $('.accordion-section-title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+ 
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+ 
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+        }
+ 
+        e.preventDefault();
+    });
+    
 });
 
